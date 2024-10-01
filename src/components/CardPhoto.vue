7fc795cf4ca2e1b97aba6067bb79b9ca1ba5bebe
Ctatianap <ctatianapabon@outlook.com>
2024-10-01 11:54:29 -0500

    card photo

---
+++ src/components/CardPhoto.vue
@@ -0,0 +1,19 @@
+<script setup>
+    const props = defineProps({
+        urlImg: {
+            type: String,
+            required: true
+        },
+    })
+    const imageSrc = new URL(`../assets/photos/${props.urlImg}`, import.meta.url).href;
+</script>
+
+<template>
+   <el-card style="max-width: 480px">
+    <img
+      :src="imageSrc"
+      class="w-[384px] h-72 object-cover"
+    />
+    <template #footer></template>
+  </el-card>
+</template>

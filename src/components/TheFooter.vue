096599463a45eb1366b9e853ca5c573ea6bcb616
Ctatianap <ctatianapabon@outlook.com>
2024-10-06 17:39:57 -0500

    add footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    Elaborado por tatiana pabon
+  <div class="bg-primary-default py-6">
+    <p class="text-cookie text-sm mx-auto text-center">Desarrollado con ♥ por Tatiana Pabón. 
+      Sígueme en 
+      <a href="https://github.com/Ctatianap" target="_blank" class="text-primary-100 mb-4">GitHub</a> y
+      <a href="https://www.linkedin.com/in/ctatianap/" target="_blank" class="text-primary-100 mb-4">LinkedIn</a>
+    </p> 
   </div>
 </template>

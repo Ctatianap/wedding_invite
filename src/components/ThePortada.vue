78a89c14568bf522f138dee72b73c06b50e80e50
Ctatianap <ctatianapabon@outlook.com>
2024-10-06 17:25:57 -0500

    add quote

--- src/components/ThePortada.vue
+++ src/components/ThePortada.vue
@@ -5,23 +5,6 @@
       required: true
     }
   })
- /*  document.addEventListener('DOMContentLoaded', () => {
-    const section = document.querySelector('#mainPage')
-    if (section) {
-      const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            const images = document.querySelectorAll('.flowers img');
-            images.forEach((img, index) => {
-              img.style.animation = `slideDown 2s ease-in-out forwards ${index * 2}s`
-            });
-          }
-        });
-      }, { threshold: 0.5 });
-
-      observer.observe(section)
-    } 
-  })*/
 </script>
 
 <template>
@@ -35,7 +18,6 @@
         <img class="" src="../assets/images/animation/top/anim-top-left-5.png" alt="leaf 5">
         <img class="" src="../assets/images/animation/top/anim-top-left-6.png" alt="leaf 6">
       </div>
-
       <div class="container mx-auto p-4 md:px-10 lg:px-20">
         <div class="mb-20 flex flex-col justify-center items-center">
           <div class="box-bride-and-groom-names w-11/12 md:w-3/4 text-center lg:p-10">
@@ -57,22 +39,13 @@
             </div>
           </div>
           <div class="flex flex-col justify-center items-center text-center py-10">
-            
-              <!-- /*<i class="fas fa-coffee"></i>*/ -->
-              <img class="h-8 w-8 mb-2" src="https://www.fixdate.io/modelo-invitacion/30/img/comilla-cierre.svg" alt="">
+              <img class="h-6 w-8 mb-2" src="../assets/images/comilla-apertura.png" alt="comilla apertura">
 
               <p class="text-lg font-light">{{ msg }}</p>
 
-              <img class="h-8 w-8 mt-2" src="https://www.fixdate.io/modelo-invitacion/30/img/comilla-cierre.svg" alt="">
-            
+              <img class="h-6 w-8 mt-2" src="../assets/images/comilla-cierre.png" alt="comilla apertura">            
           </div>
-          <!-- <div class="flex justify-content-center align-items-center">
-            <div class="flecha-continuar">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 607 607" width="607" height="607" preserveAspectRatio="xMidYMid meet" style="width: 100%; height: 100%; transform: translate3d(0px, 0px, 0px);"><defs><clipPath id="__lottie_element_111"><rect width="607" height="607" x="0" y="0"></rect></clipPath></defs><g clip-path="url(#__lottie_element_111)"><g transform="matrix(-1,0,0,-1,303.50799560546875,441.7010192871094)" opacity="1" style="display: block;"><g opacity="1" transform="matrix(1,0,0,1,0,0)"><path stroke-linecap="butt" stroke-linejoin="miter" fill-opacity="0" stroke-miterlimit="10" stroke="rgb(255,255,255)" stroke-opacity="1" stroke-width="20" d=" M-210.9709930419922,120.15899658203125 C-210.9709930419922,120.15899658203125 -0.00800000037997961,-120.33300018310547 -0.00800000037997961,-120.33300018310547 C-0.00800000037997961,-120.33300018310547 210.9709930419922,120.33300018310547 210.9709930419922,120.33300018310547"></path></g></g><g style="display: none;" transform="matrix(-1,0,0,-1,426,323.91998291015625)" opacity="1"><g opacity="1" transform="matrix(1,0,0,1,0,0)"><path stroke-linecap="butt" stroke-linejoin="miter" fill-opacity="0" stroke-miterlimit="10" stroke="rgb(255,255,255)" stroke-opacity="1" stroke-width="20" d=" M102.42900085449219,244.19000244140625 C107.41100311279297,244.593994140625 112.43499755859375,244.8489990234375 117.49600219726562,244.9499969482422"></path></g></g><g style="display: none;" transform="matrix(-1,0,0,-1,181,323.91998291015625)" opacity="1"><g opacity="1" transform="matrix(1,0,0,1,0,0)"><path stroke-linecap="butt" stroke-linejoin="miter" fill-opacity="0" stroke-miterlimit="10" stroke="rgb(255,255,255)" stroke-opacity="1" stroke-width="20" d=" M-102.42900085449219,244.19000244140625 C-107.41100311279297,244.593994140625 -112.43499755859375,244.8489990234375 -117.49600219726562,244.9499969482422"></path></g></g><g style="display: none;" transform="matrix(-0.6474335193634033,-0.9598596692085266,0.9598596692085266,-0.6474335193634033,219.33364868164062,163.1382598876953)" opacity="1"><g opacity="1" transform="matrix(1,0,0,1,0,0)"><path stroke-linecap="butt" stroke-linejoin="miter" fill-opacity="0" stroke-miterlimit="10" stroke="rgb(255,255,255)" stroke-opacity="1" stroke-width="5" d=" M126.25299835205078,-44.22999954223633 C127.17500305175781,-38.849998474121094 127.93199920654297,-33.415000915527344 128.51800537109375,-27.92799949645996"></path></g></g><g style="display: none;" transform="matrix(0.9484142065048218,0.6640867590904236,-0.6640867590904236,0.9484142065048218,422.7938537597656,382.2122802734375)" opacity="1"><g opacity="1" transform="matrix(1,0,0,1,0,0)"><path stroke-linecap="butt" stroke-linejoin="miter" fill-opacity="0" stroke-miterlimit="10" stroke="rgb(255,255,255)" stroke-opacity="1" stroke-width="5" d=" M-85.7699966430664,-256.25299072265625 C-91.1500015258789,-257.17498779296875 -96.58499908447266,-257.9320068359375 -102.0719985961914,-258.51800537109375"></path></g></g><g style="display: none;" transform="matrix(-1,0,0,-1,303.50799560546875,304.2050476074219)" opacity="1"><g opacity="1" transform="matrix(1,0,0,1,0,0)"><path stroke-linecap="butt" stroke-linejoin="miter" fill-opacity="0" stroke-miterlimit="10" stroke="rgb(255,255,255)" stroke-opacity="1" stroke-width="20" d=" M-210.9709930419922,120.15899658203125 C-210.9709930419922,120.15899658203125 -0.00800000037997961,-120.33300018310547 -0.00800000037997961,-120.33300018310547 C-0.00800000037997961,-120.33300018310547 205.0749969482422,113.60700225830078 210.8470001220703,120.19100189208984"></path></g></g></g></svg></div>
-          </div> -->
-
         </div>
-
       </div>
       <div class="flowers f-rigth">
         <img class="" src="../assets/images/animation/right/anim-bottom-right-1.png" alt="leaf-right 1">
